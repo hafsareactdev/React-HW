572a4fc3e61bc79d248439b2a23aa0a9cf8e7f6b
hafsareactdev <hafsa03bd@gmail.com>
2024-10-21 16:45:49 +0600

    react hw

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Loginform from './Components/Login/Loginform'
+
+function App() {
+ 
+  return (
+    <>
+    <Loginform></Loginform>
+    </>
+  )
+}
+export default App
---
+++ src/Components/Login/Loginform.jsx
@@ -0,0 +1,22 @@
+const Loginform = () => {
+  return (
+    <div>
+        <h1>Hello !</h1>
+    <div>
+    <h1>This is an input</h1>
+      <input type="text" />
+      <button>run</button>
+    </div>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+       Adipisci inventore provident dicta quaerat minus veniam officiis facilis nisi voluptate rem quidem nihil voluptatum quibusdam,
+       consequuntur excepturi doloribus porro, aspernatur quas quia neque exercitationem illum pariatur iusto! Consequatur mollitia facere nam, 
+       similique architecto doloremque hic dignissimos, aut libero adipisci, animi quas. Facere eaque reiciendis dolor, 
+       dicta velit modi iure consequuntur ea nam ab alias unde a consequatur accusamus impedit. 
+       Ipsum nobis facere culpa. Ut fuga ipsum aperiam temporibus esse! Tempore iste esse exercitationem, distinctio voluptatibus quas soluta sint ad eius magni ea nisi,
+       incidunt voluptas assumenda? Quo doloremque tempore harum iure!</h1>
+    <img src="log.png" alt="png" />
+    <h1>Byeeeee*****</h1>
+    </div>
+  )
+}
+export default Loginform
